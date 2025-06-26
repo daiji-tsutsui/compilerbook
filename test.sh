c62dbe425df952a09c7c5dea7f987044087bcdb2
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2025-06-27 00:25:41 +0900

    Initial commit

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+assert() {
+    expected="$1"
+    input="$2"
+
+    result=$(execute_9cc "$input")
+
+    if [ "$result" = "$expected" ]; then
+        echo "$input => $result"
+    else
+        echo "$input => $expected expected, but got $result"
+        exit 1
+    fi
+}
+
+execute_9cc() {
+    ./9cc "$1" > tmp.s
+    cc tmp.s -o tmp
+    ./tmp
+    echo "$?"
+}
+
+assert 0 0
+assert 42 42
+
+echo OK
