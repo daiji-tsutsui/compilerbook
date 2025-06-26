2eff1838f1de7d17200148a6ea05e9a0d79584e9
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2025-06-27 01:01:40 +0900

    足し算と引き算に対応

--- 9cc.c
+++ 9cc.c
@@ -1,16 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_prefix();
+void print_first_term(char** p);
+void print_add_term(char** p);
+void print_sub_term(char** p);
+void print_return();
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
 
+    char* p = argv[1];
+
+    print_prefix();
+    print_first_term(&p);
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            print_add_term(&p);
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            print_sub_term(&p);
+            continue;
+        }
+
+        fprintf(stderr, "Unexpected character!!: '%c'\n", *p);
+        return 1;
+    }
+
+    print_return();
+    return 0;
+}
+
+void print_prefix() {
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
+}
+
+void print_first_term(char** p) {
+    printf("  mov rax, %ld\n", strtol(*p, &*p, 10));
+}
+
+void print_add_term(char** p) {
+    printf("  add rax, %ld\n", strtol(*p, &*p, 10));
+}
+
+void print_sub_term(char** p) {
+    printf("  sub rax, %ld\n", strtol(*p, &*p, 10));
+}
+
+void print_return() {
     printf("  ret\n");
-    return 0;
 }
